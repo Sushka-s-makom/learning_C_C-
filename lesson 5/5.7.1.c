057bedf56fe9197c68687855dcaeec6c9c0467ec
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2025-12-20 17:47:36 +0300

    Строки начало

---
+++ lesson 5/5.7.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char str[100] = { 'B', 'e', 's', 't', ' ', 's', 't', 'r', 'i', 'n', 'g', '!', '\0' };
+    str[11] = '.';
+
+
+
+
+
+    return 0;
+}
---
+++ lesson 5/5.7.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+   char* text[3] = {"language C", "language C++" , "language Python"};
+
+   
+
+
+
+    return 0;
+}
