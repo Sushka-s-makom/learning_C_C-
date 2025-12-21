fe32605c6b718d846d6a20c091a3616bd9c8d941
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2025-12-21 10:43:39 +0300

    Объявление и вызов функций

---
+++ lesson 6/6.1.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int fact(int n)
+{ 
+    if(n == 0){ 
+            return 1;
+        }
+    if(n < 0){
+        return -1;
+    }
+    int res = 1;
+    for( int i = 1; i <= n; i ++){
+        res *= i;
+    }
+
+    return res;
+}
+
+int main(void)
+{
+    printf("%d", fact(6));
+
+    return 0;
+}
