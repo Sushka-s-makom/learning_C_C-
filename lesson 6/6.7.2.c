946624f2fd8fc28c9fae4f3bd9ffd5a7b887164f
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2025-12-25 15:40:13 +0300

    Reapply "Add recursive functions"
    This reverts commit ee1990c81efd9ec1d107822af26c1b2339fb4630.
    
    Revert recursive solutions from lesson 6

---
+++ lesson 6/6.7.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAX_LENGTH    20
+
+
+size_t range_to_ar(int* ptr_a, size_t max_len, int from, int to, size_t count)
+{ 
+    if( from > to || count >= max_len){ 
+        return count;
+    }
+
+    ptr_a[count] = from;
+    return range_to_ar(ptr_a, max_len, from +  1,to, count + 1);
+}
+int main(void)
+{
+    int max_rec;
+    int ar[MAX_LENGTH];
+    scanf("%d", &max_rec);
+
+    size_t cnt = range_to_ar(ar, MAX_LENGTH, 1, max_rec, 0);
+
+    for( int i = 0; i < cnt; i++){ 
+        printf("%d ", ar[i]);
+    }
+
+    return 0;
+}
