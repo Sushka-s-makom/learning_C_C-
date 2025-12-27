6db1f4c3191ea2deece8a29599281ca691bd5f2b
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2025-12-27 03:15:54 +0300

    Конспект по функциям с произвольным числом параметров

---
+++ lesson 6/6.8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum(int count, ...)
+{ 
+    int s = 0; 
+    va_list arg;
+    va_start(arg, count);
+    for( int i = 0; i < count; i++){ 
+        s += va_arg(arg, int);
+    }
+    va_end(arg);
+    return s;
+
+}
+
+int main(void)
+{ 
+    int res = sum (5,  1, 2, 3, 4, 5);
+    printf("%d", res);
+    return 0;
+
+}
